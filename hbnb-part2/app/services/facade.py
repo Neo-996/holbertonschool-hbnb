6ea321407366162b0fd92458d68e806bd7230517
root <abdulelahmalshehri@gmail.com>
2025-06-23 16:56:44 +0000

    updated

--- hbnb-part2/app/services/facade.py
+++ hbnb-part2/app/services/facade.py
@@ -1,10 +1,92 @@
+from app.models.user import User
+from app.models.place import Place
+from app.models.review import Review
 from app.models.amenity import Amenity
 from app.persistence.repository import InMemoryRepository
 
+
 class HBnBFacade:
     def __init__(self):
+        self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
+    # ---------------------
+    # User Methods
+    # ---------------------
+    def create_user(self, user_data):
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_all_users(self):
+        return self.user_repo.get_all()
+
+    def update_user(self, user_id, user_data):
+        user = self.user_repo.get(user_id)
+        if not user:
+            return None
+        user.update(user_data)
+        return user
+
+    def delete_user(self, user_id):
+        return self.user_repo.delete(user_id)
+
+    # ---------------------
+    # Place Methods
+    # ---------------------
+    def create_place(self, place_data):
+        place = Place(**place_data)
+        self.place_repo.add(place)
+        return place
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        place = self.place_repo.get(place_id)
+        if not place:
+            return None
+        place.update(place_data)
+        return place
+
+    def delete_place(self, place_id):
+        return self.place_repo.delete(place_id)
+
+    # ---------------------
+    # Review Methods
+    # ---------------------
+    def create_review(self, review_data):
+        review = Review(**review_data)
+        self.review_repo.add(review)
+        return review
+
+    def get_review(self, review_id):
+        return self.review_repo.get(review_id)
+
+    def get_all_reviews(self):
+        return self.review_repo.get_all()
+
+    def update_review(self, review_id, review_data):
+        review = self.review_repo.get(review_id)
+        if not review:
+            return None
+        review.update(review_data)
+        return review
+
+    def delete_review(self, review_id):
+        return self.review_repo.delete(review_id)
+
+    # ---------------------
+    # Amenity Methods
+    # ---------------------
     def create_amenity(self, amenity_data):
         amenity = Amenity(**amenity_data)
         self.amenity_repo.add(amenity)
@@ -22,3 +104,6 @@ class HBnBFacade:
             return None
         amenity.update(amenity_data)
         return amenity
+
+    def delete_amenity(self, amenity_id):
+        return self.amenity_repo.delete(amenity_id)
