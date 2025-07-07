d3e735a701c6668416752fcc888e1197dce45617
root <abdulelahmalshehri@gmail.com>
2025-07-07 14:47:43 +0000

    updated

--- part3/config.py
+++ part3/config.py
@@ -1,9 +1,15 @@
-#!/usr/bin/python3
 import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default_jwt_secret_key')
     DEBUG = False
+
+    # Optional but useful JWT settings
+    JWT_ACCESS_TOKEN_EXPIRES = 3600  # 1 hour (in seconds)
+    JWT_TOKEN_LOCATION = ['headers']  # Default, but explicit
+    JWT_HEADER_NAME = 'Authorization'
+    JWT_HEADER_TYPE = 'Bearer'
 
 class DevelopmentConfig(Config):
     DEBUG = True
@@ -12,3 +18,4 @@ config = {
     'development': DevelopmentConfig,
     'default': DevelopmentConfig
 }
+
