1fe4b3d742102ab11219f16e55e8b3a488a55306
Muhannad-09 <muhannadgsgs@gmail.com>
2025-07-09 19:40:48 +0000

    app/__init.py__

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,21 +1,26 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from config import config
 
 # Import your API namespaces here
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.auth import api as auth_ns
 # Import other namespaces as needed (places, reviews, etc.)
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_name='default'):
+    """Application Factory to initialize Flask app with configuration"""
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
-    # Initialize bcrypt with the app
+    # Initialize extensions
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     # Initialize Flask-Restx API
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
@@ -23,7 +28,8 @@ def create_app(config_name='default'):
     # Register namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    # Register other namespaces here
+    api.add_namespace(auth_ns, path='/api/v1/auth')
+    # Register other namespaces here (places, reviews, etc.)
 
     return app
 
