db9bf30435e6850d4eb7f95a8f080dc482282cec
Muhannad-09 <muhannadgsgs@gmail.com>
2025-07-09 20:19:30 +0000

    update __init.py__

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -4,11 +4,12 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from config import config
 
-# Import your API namespaces here
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
+# Import API namespaces
 from app.api.v1.auth import api as auth_ns
-# Import other namespaces as needed (places, reviews, etc.)
+from app.api.v1.users import api as users_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.amenities import api as amenities_ns
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
@@ -25,11 +26,12 @@ def create_app(config_name='default'):
     # Initialize Flask-Restx API
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
-    # Register namespaces
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    # Register namespaces with their URL prefixes
     api.add_namespace(auth_ns, path='/api/v1/auth')
-    # Register other namespaces here (places, reviews, etc.)
+    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
     return app
 
