7c55577c3cc506b14b0c4a569bc411d4e9bc8a7e
Muhannad-09 <muhannadgsgs@gmail.com>
2025-07-10 19:33:56 +0000

    SQLAlchemy __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,10 +1,13 @@
+# File: app/__init__.py
+
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 from config import config
 
-# Import API namespaces
+# API Namespaces
 from app.api.v1.auth import api as auth_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
@@ -13,20 +16,18 @@ from app.api.v1.amenities import api as amenities_ns
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
+db = SQLAlchemy()
 
 def create_app(config_name='default'):
-    """Application Factory to initialize Flask app with configuration"""
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
-    # Initialize extensions
     bcrypt.init_app(app)
     jwt.init_app(app)
+    db.init_app(app)
 
-    # Initialize Flask-Restx API
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
-    # Register namespaces with their URL prefixes
     api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(places_ns, path='/api/v1/places')
